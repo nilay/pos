e0a0170950c70d1ba96be23c8f5d15ce78f42117
Nilay <nilayanand@gmail.com>
2016-12-01 14:28:23 +0530

    CRUD for Category object

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ActiveRecord::Base
   has_many :dishes
 
   validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :dashboards
   resources :customers
   resources :tables
+  resources :categories
 
 end
